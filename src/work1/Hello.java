ac42251a3e25d9bf87c3c8ebcf1192a18b978a78
Selda <seldahas.sh@gmail.com>
2021-09-16 14:32:10 +0300

    Added more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -5,6 +5,6 @@ public class Hello {
 
         System.out.println("Work1 started");
         System.out.println("Created Git Repository");
-
+        System.out.println("Made few commits");
     }
 }
